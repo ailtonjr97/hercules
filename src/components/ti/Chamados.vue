1bf316b3991331067d0d8520134ef8949d99d795
Ailton <ailton.souza97@hotmail.com>
2023-12-15 10:30:41 -0300

    ver cards chamados

--- src/components/ti/Chamados.vue
+++ src/components/ti/Chamados.vue
@@ -1,61 +1,102 @@
 <template>
-    <div v-if="carregando" id="loading"></div>
+
+<div v-if="carregando" id="loading"></div>
         <div v-if="fullLoad" style="overflow: hidden; padding: 0.5%;">
-        <table-top :resultados="resultados">
-            <template v-slot:tableButtons>
-                <button class="button-8 mb-2" @click="refresh">Atualizar</button>
-            </template>
-        </table-top>
-        <div class="row mb-2">
-            <table-search :id="'procuraBtn0'" :num="0" :placeholder="'ID:'"></table-search>
-            <table-search :id="'procuraBtn1'" :num="1" :placeholder="'Requisitante:'"></table-search>
-            <table-search :id="'procuraBtn2'" :num="2" :placeholder="'Designado:'"></table-search>
-            <table-search :id="'procuraBtn3'" :num="3" :placeholder="'Descrição:'"></table-search>
-        </div>
-        <div class="table-wrapper table-responsive table-striped mb-5">
-            <table class="fl-table" id="myTable">
-            <thead>
-                <tr style="height: 25px">
-                <th>ID</th>
-                <th>Requisitante</th>
-                <th>Designado</th>
-                <th>Área</th>
-                <th>Tipo</th>
-                <th>Descrição</th>
-                <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="chamado in chamados" :key="chamado.id">
-                <td>
-                    <p v-if="chamado.urgencia == 2" style="background-color: yellow; border-radius: 5px">{{chamado.id}}</p>
-                    <p v-if="chamado.urgencia == 3" style="background-color: red; border-radius: 5px">{{chamado.id}}</p>
-                    <p v-else>{{chamado.id}}</p>
-                </td>
-                <td>
-                    <p>{{ chamado.name }}</p>
-                </td>
-                <td>
-                    <p>{{ chamado.designado}}</p>
-                </td>
-                <td>
-                    <p>{{ chamado.area}}</p>
-                </td>
-                <td>
-                    <p>{{ chamado.operacoes}}</p>
-                </td>
-                <td style="white-space: normal; word-break: break-word">
-                    <p v-if="chamado.descricao.length <= 180">{{ chamado.descricao}}</p>
-                    <p v-else>{{ chamado.descricao.substring(0, 180)}}... <a @click="openModalDescricao(chamado.descricao)" style="color: #0d6efd; text-decoration: underline; cursor: pointer;">(ver mais)</a></p>
-                </td>
-                <td>
-                    <button class="button-8" @click="verChamado(chamado.id)"><i class="fa-solid fa-eye" style="font-size: 14px;"></i></button>
-                    <button class="button-8" @click="editarChamado(chamado.id)"><i class="fa-solid fa-pen-to-square" style="font-size: 14px;"></i></button>
-                </td>
-                </tr>
-            </tbody>
-            </table>
-        </div>
+
+            <div v-if="modoCard">
+                <table-top :resultados="resultados">
+                    <template v-slot:tableButtons>
+                        <button class="button-8 mb-2" @click="refresh">Atualizar</button>
+                        <button class="button-8" @click="mudarModoTabela">Ver em tabela</button>
+                    </template>
+                </table-top>
+                <div class="row">
+                    <div class="col-md-4 mb-5 d-flex justify-content-evenly" v-for="chamado in chamados" :key="chamado.id">
+                        <div class="card" style="width: 31rem; background-color: #dfdfdf;">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-lg-9">
+                                        <h5 style="margin-top: 2%;">{{ chamado.designado }}</h5>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <button v-if="chamado.urgencia == 1" type="button" class="btn btn-primary" style="margin-left: 30%;">{{ chamado.id }}</button>
+                                        <button v-if="chamado.urgencia == 3" type="button" class="btn btn-warning" style="margin-left: 30%;">{{ chamado.id }}</button>
+                                        <button v-if="chamado.urgencia == 4" type="button" class="btn btn-danger" style="margin-left: 30%;">{{ chamado.id }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <h6 v-if="chamado.descricao.length <= 255">{{ chamado.descricao}}</h6>
+                                <h6 v-else>{{ chamado.descricao.substring(0, 255)}}... <a @click="openModalDescricao(chamado.descricao)" style="color: #0d6efd; text-decoration: underline; cursor: pointer;">(ver mais)</a></h6>
+                                <p class="card-text">- {{ chamado.name }}</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <button style="font-size: 14px; width: 10%; left: 85%;" class="button-8 mt-2 mb-2" @click="editarChamado(chamado.id)"><i class="fa-solid fa-pen-to-square"></i></button>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div v-if="modoTabela">
+                <table-top :resultados="resultados">
+                    <template v-slot:tableButtons>
+                        <button class="button-8 mb-2" @click="refresh">Atualizar</button>
+                        <button class="button-8" @click="mudarModoCard">Ver em cartão</button>
+                    </template>
+                </table-top>
+                <div class="row mb-2">
+                    <table-search :id="'procuraBtn0'" :num="0" :placeholder="'ID:'"></table-search>
+                    <table-search :id="'procuraBtn1'" :num="1" :placeholder="'Requisitante:'"></table-search>
+                    <table-search :id="'procuraBtn2'" :num="2" :placeholder="'Designado:'"></table-search>
+                    <table-search :id="'procuraBtn3'" :num="3" :placeholder="'Descrição:'"></table-search>
+                </div>
+                <div class="table-wrapper table-responsive table-striped mb-5">
+                    <table class="fl-table" id="myTable">
+                    <thead>
+                        <tr style="height: 25px">
+                        <th>ID</th>
+                        <th>Requisitante</th>
+                        <th>Designado</th>
+                        <th>Área</th>
+                        <th>Tipo</th>
+                        <th>Descrição</th>
+                        <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="chamado in chamados" :key="chamado.id">
+                        <td>
+                            <p v-if="chamado.urgencia == 1">{{chamado.id}}</p>
+                            <p v-if="chamado.urgencia == 3" style="background-color: yellow; border-radius: 5px">{{chamado.id}}</p>
+                            <p v-if="chamado.urgencia == 4" style="background-color: red; border-radius: 5px">{{chamado.id}}</p>
+                        </td>
+                        <td>
+                            <p>{{ chamado.name }}</p>
+                        </td>
+                        <td>
+                            <p>{{ chamado.designado}}</p>
+                        </td>
+                        <td>
+                            <p>{{ chamado.area}}</p>
+                        </td>
+                        <td>
+                            <p>{{ chamado.operacoes}}</p>
+                        </td>
+                        <td style="white-space: normal; word-break: break-word">
+                            <p v-if="chamado.descricao.length <= 180">{{ chamado.descricao}}</p>
+                            <p v-else>{{ chamado.descricao.substring(0, 180)}}... <a @click="openModalDescricao(chamado.descricao)" style="color: #0d6efd; text-decoration: underline; cursor: pointer;">(ver mais)</a></p>
+                        </td>
+                        <td>
+                            <button class="button-8" @click="verChamado(chamado.id)"><i class="fa-solid fa-eye" style="font-size: 14px;"></i></button>
+                            <button class="button-8" @click="editarChamado(chamado.id)"><i class="fa-solid fa-pen-to-square" style="font-size: 14px;"></i></button>
+                        </td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
     </div>
 
     <modal v-if="modalVerChamado" :title="'Dados do chamado:'">
@@ -135,6 +176,8 @@ export default {
     },
     data(){
         return{
+            modoTabela: false,
+            modoCard: true,
             department_id: null,
             modalDescricaoText: '',
             modalDescricao: false,
@@ -160,6 +203,14 @@ export default {
         }
     },
     methods: {
+        async mudarModoCard(){
+            this.modoCard = true;
+            this.modoTabela = false;
+        },
+        async mudarModoTabela(){
+            this.modoCard = false;
+            this.modoTabela = true;
+        },
         async closeModalEditarChamado(){
             this.modalEditarChamado = false;
             this.designadoOptions = [],
