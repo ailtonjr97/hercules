aa94fc27377264fa45f7f6ef678590683432357e
Ailton <ailton.souza97@hotmail.com>
2024-01-19 16:08:01 -0300

    add home descri

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>Home</h1>
+    <h1>Bem-vindo ao integrador Korp da Fibracem.</h1>
+    <h6>Aqui você poderá ter acesso a algumas informações do Korp sem precisar logar no sistema.</h6>
+    <h6>Basta clicar em "Korp" na barra acima para ter acesso. Mais funcionalidades serão criadas com o tempo.</h6>
 </template>
