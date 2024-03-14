331aa3c6d5aed78881fa6bdff648b9dfaf2b5558
Ailton <ailton.souza97@hotmail.com>
2024-03-14 17:42:08 -0300

    terminar vendedores

--- src/components/comercial/ComercialVendedores.vue
+++ src/components/comercial/ComercialVendedores.vue
@@ -12,6 +12,7 @@
         <table-search :id="'procuraBtn1'" :num="1" :placeholder="'Filial:'"></table-search>
         <table-search :id="'procuraBtn2'" :num="2" :placeholder="'Código:'"></table-search>
         <table-search :id="'procuraBtn3'" :num="3" :placeholder="'Nome:'"></table-search>
+        <table-search :id="'procuraBtn4'" :num="4" :placeholder="'E-mail:'"></table-search>
     </div>
     <div class="table-wrapper table-responsive table-striped mb-5">
         <table class="fl-table" id="myTable">
@@ -21,6 +22,8 @@
             <th>Filial</th>
             <th>Código</th>
             <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -29,11 +32,65 @@
             <td><p>{{ api.filial }}</p></td>
             <td><p>{{ api.cod }}</p></td>
             <td><p>{{ api.nome }}</p></td>
+            <td><p>{{ api.email }}</p></td>
+            <td>
+                <button title="Editar" class="button-8" @click="openEditarModal(api.id)"><i style="font-size: 14px;" class="fa-solid fa-pen"></i></button>
+            </td>
             </tr>
         </tbody>
         </table>
     </div>
 </div>
+
+<modal v-if="vendedorModal" :title="`${vendedor[0].nreduz}`">
+    <template v-slot:body>
+    <loading v-if="carregandoinfo"></loading>
+    <div v-if="!carregandoinfo">
+        <div class="row mb-2">
+            <div class="col-sm-1">
+                <form-floating :placeholder="'Filial:'" :id="'filial'" :type="'text'" v-model="vendedor[0].filial" readonly></form-floating>
+            </div>
+            <div class="col-sm-1">
+                <form-floating :placeholder="'Código:'" :id="'cod'" :type="'text'" v-model="vendedor[0].cod" readonly></form-floating>
+            </div>
+            <div class="col">
+                <form-floating v-bind:style= "[vendedor[0].nome.toString().length > 40 || vendedor[0].nome.toString().length < 3 ? {'color': 'red'} : {'color': 'inherit'}]" :placeholder="`(${vendedor[0].nome.toString().length}) Nome:`" :id="'nome'" :type="'text'" v-model="vendedor[0].nome"></form-floating>
+            </div>
+            <div class="col-sm-3">
+                <form-floating v-bind:style= "[vendedor[0].email.toString().length > 100 || vendedor[0].email.toString().length < 3 ? {'color': 'red'} : {'color': 'inherit'}]" :placeholder="`(${vendedor[0].email.toString().length}) E-mail:`" :id="'email'" :type="'text'" v-model="vendedor[0].email"></form-floating>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2">
+                <form-floating :placeholder="'Endereço:'" :id="'end'" :type="'text'" v-model="vendedor[0].end"></form-floating>
+            </div>
+            <div class="col-sm-2">
+                <form-floating :placeholder="'Bairro:'" :id="'bairro'" :type="'text'" v-model="vendedor[0].bairro"></form-floating>
+            </div>
+            <div class="col-sm-2">
+                <form-floating :placeholder="'Município:'" :id="'mun'" :type="'text'" v-model="vendedor[0].mun"></form-floating>
+            </div>
+            <div class="col-sm-1">
+                <form-floating :placeholder="'Estado:'" :id="'est'" :type="'text'" v-model="vendedor[0].est"></form-floating>
+            </div>
+            <div class="col">
+                <form-floating :placeholder="'CEP:'" :id="'cep'" :type="'text'" v-model="vendedor[0].cep"></form-floating>
+            </div>
+            <div class="col-sm-1">
+                <form-floating :placeholder="'DDD:'" :id="'dddtel'" :type="'text'" v-model="vendedor[0].dddtel"></form-floating>
+            </div>
+            <div class="col">
+                <form-floating :placeholder="'Telefone:'" :id="'tel'" :type="'text'" v-model="vendedor[0].tel"></form-floating>
+            </div>
+        </div>
+    </div>
+    </template>
+    <template v-slot:buttons v-if="!carregandoinfo">
+        <button class="button-8 mt-2" @click="fecharVendedorModal()">Fechar</button>
+        <button class="button-8 mt-2" @click="salvarVendedorModal()">Salvar</button>
+    </template>
+</modal>
+
 </template>
 
 <script>
@@ -67,26 +124,60 @@ components: {
 },
 data(){
     return{
+        vendedorModal: false,
         popup: false,
-        disableBtn: false,
-        carregandoinfoApi: false,
         carregandoinfo: false,
-        modalRelatorio: false,
         resultados: 0,
         carregando: true,
         apis: [],
-        modalApi: false,
         carregandoinfo: false,
-        visualizar: {},
-        dadosRelatorio: {},
-        lista: {
-            metodo: '',
-            descricao: '',
-            caminho: ''
-        }
+        vendedor: {}
     }
 },
 methods: {
+    async salvarVendedorModal(){
+        try {
+            if(this.vendedor[0].nome.toString().length > 40 || this.vendedor[0].nome.toString().length < 3 ||
+            this.vendedor[0].email.toString().length > 100 || this.vendedor[0].email.toString().length < 3){
+                alert("Favor preencher de forma correta os campos em vermelho.");
+                return null;
+            }else{
+                this.vendedorModal = false;
+                this.carregando = true;
+                let jsonCliente = {};
+                jsonCliente = {
+                    "A3_FILIAL": this.vendedor[0].filial, 
+                    "A3_COD": this.vendedor[0].cod, 
+                    "A3_NOME": this.vendedor[0].nome,
+                    "A3_EMAIL": this.vendedor[0].email
+                };
+                await axios.post(`${import.meta.env.VITE_BACKEND_IP}/comercial/sa3/api/update`, jsonCliente, config);
+                jsonCliente = {};
+                this.refresh()
+            }
+        } catch (error) {
+            jsonCliente = {};
+            this.carregando = false;
+            this.carregandoinfo = false;
+            alert("Erro ao alterar cadastro do cliente. Favor tentar novamente mais tarde");
+        }
+    },
+    async openEditarModal(id){
+        try {
+            this.vendedorModal = true;
+            this.carregandoinfo = true;
+            const response = await axios.get(`${import.meta.env.VITE_BACKEND_IP}/comercial/sa3/vendedor/${id}`, config);
+            this.vendedor = response.data;
+            this.carregandoinfo = false;
+        } catch (error) {
+            alert("Falha ao mostrar informações. Favor tentar novamente mais tarde.");
+        }
+    },
+    async fecharVendedorModal(){
+        this.carregandoinfo = false;
+        this.vendedorModal = false;
+        this.vendedor = {};
+    },
     async refresh(){
         try {
             this.carregando = true;
